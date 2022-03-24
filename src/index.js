cea0bc53983bcfcf16aa364d5f022ee897f4ddc8
Daniel Gorianz <aalbaafikaa@gmail.com>
2022-03-23 21:35:01 -0400

    se creo un custom Hook para el manejo del formulario

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FormWithCustomHook from './componets/02-useEffect/FormWithCustomHook';
 // import CounterApp from './componets/01-useState/CounterApp';
-import CounterWhitCustomHook from './componets/01-useState/CounterWhitCustomHook';
+// import CounterWhitCustomHook from './componets/01-useState/CounterWhitCustomHook';
+// import SimpleForm from './componets/02-useEffect/SimpleForm';
 // import HookApp from './HookApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CounterWhitCustomHook />
+    <FormWithCustomHook />
   </React.StrictMode>,
   document.getElementById('root')
 );
